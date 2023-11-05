bc5f07c7812987b45eb40ed7b6e6d90449d384e5
NataliaDudina <dudina.natalja@rambler.ru>
2023-11-06 00:11:51 +0300

    styles fixed

--- homework_1/main.py
+++ homework_1/main.py
@@ -1,4 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
 
 host = 'localhost'
 server_port = 8080
@@ -10,23 +11,20 @@ class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         """ Метод для обработки входящих GET-запросов """
 
-        if self.path == '/':
-            file_path = 'index.html'
-            try:
-                with open(file_path, 'r') as file:
-                    page_content = file.read()
-                    self.send_response(200)  # Отправка кода ответа
-                    self.send_header("Content-type", "text/html")  # Отправка типа данных, который будет передаваться
-                    self.end_headers()  # Завершение формирования заголовков ответа
-                    self.wfile.write(bytes(page_content, "utf-8"))  # Тело ответа
-            except FileNotFoundError:
-                self.send_response(404)
-                self.end_headers()
-                self.wfile.write(bytes("File not found", "utf-8"))
-        else:
+        query_components = parse_qs(urlparse(self.path).query)
+        print(query_components)    # для POST-запросов
+
+        try:
+            with open('index.html', 'r') as file:
+                page_content = file.read()
+                self.send_response(200)  # Отправка кода ответа
+                self.send_header("Content-type", "text/html")  # Отправка типа данных, который будет передаваться
+                self.end_headers()  # Завершение формирования заголовков ответа
+                self.wfile.write(bytes(page_content, "utf-8"))  # Тело ответа
+        except FileNotFoundError:
             self.send_response(404)
             self.end_headers()
-            self.wfile.write(bytes("Page not found", "utf-8"))
+            self.wfile.write(bytes("File not found", "utf-8"))
 
 
 if __name__ == '__main__':
